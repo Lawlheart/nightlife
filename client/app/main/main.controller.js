03ec63b91ee92a028477ff6e293bf831f5d6ac6a
LawlietBlack <kenneth.black@live.com>
2015-10-02 06:41:57 -0700

    Added icons representing the mean stack and the beginnings of testing and the yelp.js file

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,22 +1,10 @@
 'use strict';
 
 angular.module('nightlifeApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+.controller('MainCtrl', function ($scope, $http, Auth) {
+  $scope.yelp = function(num) {
+    return num
+  }
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-  });
+});
--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -1,28 +1,35 @@
 'use strict';
 
 describe('Controller: MainCtrl', function () {
+  //Karma setup
+    // load the controller's module
+    beforeEach(module('nightlifeApp'));
 
-  // load the controller's module
-  beforeEach(module('nightlifeApp'));
+    var MainCtrl,
+        scope,
+        $httpBackend;
 
-  var MainCtrl,
-      scope,
-      $httpBackend;
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('/api/things')
+        .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/things')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+      scope = $rootScope.$new();
+      MainCtrl = $controller('MainCtrl', {
+        $scope: scope
+      });
+    }));
 
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+    it('should attach a list of things to the scope', function () {
+      $httpBackend.flush();
+      expect(scope.awesomeThings.length).toBe(4);
     });
-  }));
 
-  it('should attach a list of things to the scope', function () {
-    $httpBackend.flush();
-    expect(scope.awesomeThings.length).toBe(4);
-  });
+  describe('The yelp function', function() {
+    it('should return something', function() {
+      expect(scope.yelp(5)).toBe(5);
+    });
+  })
+
 });
